e523dacee3877e506e775eab45e7da89b974b4bb
Gergely Mezei <mezei.gergely89@gmail.com>
2020-12-21 10:11:02 +0100

    getOne view added

---
+++ resources/views/getOne.blade.php
@@ -0,0 +1,8 @@
+<h3>{{$post->title}}</h3>
+<p>{{$post->date}}</p>
+<p>{{$post->short_desc}}</p>
+<div>
+    <img src="{{$post->thumbnail}}" height="400">
+</div>
+<p>{{$post->long_desc}}</p>
+<a href="/">back</a>
--- resources/views/newsfeed.blade.php
+++ resources/views/newsfeed.blade.php
@@ -2,7 +2,7 @@
 <h3>{{$item->title}}</h3>
 <p>{{$item->date}}</p>
 <p>{{$item->short_desc}}</p>
-<a>Continue reading</a>
+<a href="{{$item->id}}">Continue reading</a>
 <div>
     <img src="{{$item->thumbnail}}" height="150" width="200">
 </div>

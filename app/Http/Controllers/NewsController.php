949a46b09ef0d6c1c67bcf5ab3476b40cb9e75f2
Gergely Mezei <mezei.gergely89@gmail.com>
2020-12-21 09:52:06 +0100

    newsfeed view added with datas from db

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,15 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
+    // get data from DB (reverse)
     function index()
     {
-        return "NewsController index";
+        $data = DB::table('news')->get()->reverse();
+        return view('newsfeed', ['data' => $data]);
     }
 
+    // get data from DB by id
     function get_one($id)
     {
+        $data = DB::table('news')->where('id', $id)->first();
+        return view('getOne')->with("post", $data);
     }
 }
---
+++ resources/views/newsfeed.blade.php
@@ -0,0 +1,9 @@
+@foreach($data as $item)
+<h3>{{$item->title}}</h3>
+<p>{{$item->date}}</p>
+<p>{{$item->short_desc}}</p>
+<a>Continue reading</a>
+<div>
+    <img src="{{$item->thumbnail}}" height="150" width="200">
+</div>
+@endforeach

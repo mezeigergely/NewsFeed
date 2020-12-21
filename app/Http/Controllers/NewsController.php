a8a368ce04a1a5b8d19b1f6259e8a4c5e11ec446
Gergely Mezei <mezei.gergely89@gmail.com>
2020-12-21 09:40:27 +0100

    NewsController added

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    function index()
+    {
+        return "NewsController index";
+    }
+
+    function get_one($id)
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'NewsController@index');
+Route::get('/{id}', 'NewsController@get_one');
